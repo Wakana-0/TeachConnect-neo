e2eae979adf085d24730b04460502500e9a82ea1
Wakana-0 <oyr13824761386@outlook.com>
2025-05-24 02:22:50 +0800

    feat: 完善主界面

--- Main.py
+++ Main.py
@@ -1,15 +1,31 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton, \
-    QMessageBox, QComboBox, QLineEdit, QProgressBar, QCommandLinkButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton, QMessageBox, QComboBox, QLineEdit, QProgressBar, QCommandLinkButton
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, QDir
+from PySide6.QtGui import QIcon  # 添加QIcon导入
 from loguru import logger
+import os
+from datetime import datetime  # 添加datetime导入
 
+# 创建logs文件夹（如果它不存在）
+log_folder = './logs'
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+
+# 生成当前时间的日志文件名
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_file_name = os.path.join(log_folder, f"app_{current_time}.log")
+
+# 初始化日志记录器，将日志文件放在logs文件夹下
+logger.add(log_file_name, format="{time} {level} {message}", level="DEBUG")
+logger.info("应用程序启动")
 
 class LoginWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.load_ui()
-        self.resize(300, 175)
+        self.resize(315, 180)
+        self.setWindowTitle("TeachConnect-neo - 登录")
+        self.setWindowIcon(QIcon('./assets/icon.png'))  # 设置窗口图标
 
     def load_ui(self):
         # 创建一个QUiLoader对象
@@ -53,8 +69,10 @@ class LoginWindow(QMainWindow):
 
             # 访问account_selection
             self.account_selection = self.ui.findChild(QComboBox, "account_selection")
-            self.account_selection.clicked.connect(self.register)
 
+            # 访问go_registration
+            self.go_registration = self.ui.findChild(QCommandLinkButton, "go_registration")
+            self.go_registration.clicked.connect(self.open_register_window)
 
         else:
             logger.error("加载UI文件失败")
@@ -64,21 +82,25 @@ class LoginWindow(QMainWindow):
                                               f"您输入的账号为：{self.account_selection.currentText()}\n"
                                               f"您输入的密码为：{self.password_input.text()}")
 
-    def register(self):
-        pass
+    def open_register_window(self):
+        self.register_window = RegisterWindow()
+        self.register_window.show()
+
 
 class RegisterWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.load_ui()
-        self.resize(800, 600)
+        self.resize(340, 200)
+        self.setWindowTitle("TeachConnect-neo - 注册")
+        self.setWindowIcon(QIcon('./assets/icon.png'))  # 设置窗口图标
 
     def load_ui(self):
         # 创建一个QUiLoader对象
         loader = QUiLoader()
         logger.debug("QUiLoader对象已创建")
 
-        # 获取MainWindow.ui文件的路径
+        # 获取RegisterWindow.ui文件的路径
         ui_file_name = QDir.current().filePath('./pages/RegisterWindow.ui')
         logger.debug(f"UI文件路径: {ui_file_name}")
 
@@ -106,22 +128,42 @@ class RegisterWindow(QMainWindow):
             self.setCentralWidget(self.ui)
             logger.info("UI文件已成功加载并设置为主窗口的中心部件")
 
-            # 访问PushButton
-            self.PushButton = self.ui.findChild(QPushButton, "PushButton")
+            # 访问account_lineEdit
+            self.account_lineEdit = self.ui.findChild(QLineEdit, "account_lineEdit")
 
+            # 访问password_lineEdit
+            self.password_lineEdit = self.ui.findChild(QLineEdit, "password_lineEdit")
+
+            # 访问confirm_password_lineEdit
+            self.confirm_password_lineEdit = self.ui.findChild(QLineEdit, "confirm_password_lineEdit")
+
+            # 访问certainly_pushButton
+            self.certainly_pushButton = self.ui.findChild(QPushButton, "certainly_pushButton")
+            self.certainly_pushButton.clicked.connect(self.register)
 
         else:
             logger.error("加载UI文件失败")
 
-if __name__ == "__main__":
-    # 初始化日志记录器
-    logger.add("app.log", format="{time} {level} {message}", level="DEBUG")
-    logger.info("应用程序启动")
+    def register(self):
+        # 获取输入的账号、密码、确认密码
+        account = self.account_lineEdit.text()
+        password = self.password_lineEdit.text()
+        confirm_password = self.confirm_password_lineEdit.text()
 
+        # 判断输入的密码是否一致
+        if password != confirm_password:
+            QMessageBox.warning(self, "警告", "两次输入的密码不一致！")
+            return
+
+        QMessageBox.information(self, "提示", f"功能还在开发中\n\n账户：{account}\n密码是否一致：{password == confirm_password}")
+
+
+if __name__ == "__main__":
     # 创建QApplication对象，初始化GUI应用程序
     app = QApplication([])
+    app.setWindowIcon(QIcon('./assets/icon.png'))  # 设置应用程序图标
 
-    # 创建MainWindow对象并显示
+    # 创建LoginWindow对象并显示
     mainWin = LoginWindow()
     mainWin.show()
 
---
+++ README.md
@@ -0,0 +1,39 @@
+> [!NOTE]
+> TeachConnect 的 Discord 服务器：https://discord.gg/s5z94tGbRq
+
+<p align="center">
+  <img width="16%" align="center" src="assets/favicon.png" alt="favicon">
+</p>
+  <h1 align="center">
+  TeachConnect | 师室互联
+</h1>
+
+
+<p align="center">
+ 远程教师工具 | 方便 远程 教学
+</p>
+
+[![当前版本](https://img.shields.io/github/v/release/pyteachconnect/teachconnect?style=for-the-badge&color=purple&label=%E5%BD%93%E5%89%8D%E7%89%88%E6%9C%AC)](https://github.com/pyTeachConnect/TeachConnect/releases/tag/Beta-0.3.0)
+[![开源许可](https://img.shields.io/badge/license-GPLv3-blue.svg?label=开源许可证&style=for-the-badge)](https://github.com/RinLit-233-shiroko/Class-Widgets)
+
+
+#### [📦 前往本仓库](https://github.com/pyTeachConnect/TeachConnect)
+
+</div>
+
+## 安装 & 使用
+
+#### 确认系统需求
+首先，确认您的设备是否满足以下需求:
+
+> [!NOTE]
+> **兼容性：** 现支持 Windows 10
+
+| **🖥操作系统** | **🗄运行内存** | **运行环境** |
+| :-----: | :-----: | :------: |
+| **Windows 10** 及以上（仅限Windows） | ≥4GB | 仅限Windows |
+
+若已**满足**，请进行接下来的步骤。
+
+#### 下载 TeachConnect
+您可在此应用 GitHub 仓库中下载最新的 TeachConnect:https://github.com/pyTeachConnect/TeachConnect/releases/
